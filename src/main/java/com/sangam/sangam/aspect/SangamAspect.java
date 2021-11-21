d5a0675e46e392bc4c2822c727f75c46d41c389b
Shanmuganathan113 <shan.nathan113@gmail.com>
2021-11-21 13:23:21 +0530

    Update with latest changes

--- src/main/java/com/sangam/sangam/aspect/SangamAspect.java
+++ src/main/java/com/sangam/sangam/aspect/SangamAspect.java
@@ -7,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sangam.sangam.beans.ErrorBean;
--- src/main/java/com/sangam/sangam/web/security/config/SecurityConfig.java
+++ src/main/java/com/sangam/sangam/web/security/config/SecurityConfig.java
@@ -40,8 +40,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private CustomOAuth2UserService customOAuth2UserService;
 
-    @Autowired
-    private OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+//    @Autowired
+//    private OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
 
     @Autowired
     private OAuth2AuthenticationFailureHandler oAuth2AuthenticationFailureHandler;
@@ -96,7 +96,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-    	System.out.println("Entered");
         http
                 .cors()
                     .and()
